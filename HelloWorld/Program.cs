7f92bcda6d1af06edc7a67b66bcb9cae71370420
Test Bild <testbild@net.hr>
2018-07-18 14:27:27 +0200

    TEST2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,8 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.Write("Press any key to continue.....")
-            Console.ReadKey(true)
-        }
+            Console.Write("Press any key to continue.......");
+            Console.ReadKey(true);        }
     }
 }
